c7870bf2bf2fcca49d7370b1d8820232a240d87c
rarimoto <masae1263@gmail.com>
2020-04-21 19:37:57 -1000

    add contact button to services page

--- src/script.js
+++ src/script.js
@@ -3,4 +3,18 @@ $(document).ready(function(){
 	$('.mobile_nav').click(function(){
 		$(this).toggleClass('open', 500);
 	});
+
+	function checkOffset() {
+			if($('.services_main_button').offset().top
+					>= $('.footer').offset().top)
+					$('.services_main_button').css({'position': 'absolute', 'bottom': '-95px'});
+			if($(document).scrollTop() + window.innerHeight < $('.footer').offset().top)
+					$('.services_main_button').css({'position': 'fixed', 'bottom': '5%'});
+					// restore when you scroll up
+			$('.services_main_button').scrollTop() + window.innerHeight;
+	}
+
+		$(document).scroll(function() {
+				checkOffset();
+		});
 });
